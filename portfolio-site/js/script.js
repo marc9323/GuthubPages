f5e63084a39e86c6ec2526cc789130f1a2dc93b5
Marc D. Holman <31974365+marc9323@users.noreply.github.com>
2019-08-15 14:50:28 -0500

    update portfolio

--- portfolio-site/js/script.js
+++ portfolio-site/js/script.js
@@ -1,3 +1,20 @@
+$(window).on('load', function() {
+    // first icon fades out, then the background
+    $('.loader .inner').fadeOut(1000, function() {
+        $('.loader').fadeOut(1250);
+    });
+
+    // call isotope plugin - place here to fix image overlap bug
+    $('.items').isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
+});
+
 $(document).ready(function() {
     $('#slides').superslides({
         animation: 'fade',
@@ -20,6 +37,11 @@ $(document).ready(function() {
     $('.owl-carousel').owlCarousel({
         loop: true,
         items: 4,
+        nav: true,
+        smartSpeed: 900,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
         // screen widths
         responsive: {
             0: {
@@ -60,4 +82,58 @@ $(document).ready(function() {
             });
         }
     });
+
+    // now add data-fancybox attribute to html element
+    $('[data-fancybox]').fancybox();
+
+    $('#filters a').click(function() {
+        // remove current class from default and add to this
+        $('#filters .current').removeClass('current');
+        $(this).addClass('current');
+
+        // grab the value of the data-filter attribute
+        var selector = $(this).attr('data-filter');
+
+        // now call isotope with selector
+        $('.items').isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        // end click handler - cancel default behavior
+        return false;
+    });
+
+    $('#navigation li a').click(function(e) {
+        e.preventDefault();
+
+        // this gets the current element called on so <a> href att
+        var targetElement = $(this).attr('href');
+        var targetPosition = $(targetElement).offset().top;
+
+        $('html, body').animate({ scrollTop: targetPosition - 50 }, 'slow');
+    });
+
+    const nav = $('#navigation');
+    const navTop = nav.offset().top; // get top position of nav
+    // when nav is at the top, add a class that fixes it to the top
+
+    $(window).on('scroll', stickyNavigation);
+
+    function stickyNavigation() {
+        var body = $('body');
+
+        // if the position scrolled is past navtop
+        if ($(window).scrollTop() >= navTop) {
+            body.css('padding-top', nav.outerHeight() + 'px');
+            body.addClass('fixedNav');
+        } else {
+            body.css('padding-top', 0);
+            body.removeClass('fixedNav');
+        }
+    }
 });
